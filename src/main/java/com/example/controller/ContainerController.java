a4f94360e71e785bff3c29fe5576832860016999
Stanislav <stdu92@mail.ru>
2023-11-19 13:40:28 +0300

    default

---
+++ src/main/java/com/example/controller/ContainerController.java
@@ -0,0 +1,25 @@
+package com.example.controller;
+
+import com.example.service.ContainerService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContainerController {
+
+    ContainerService containerService;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/example/service/ContainerService.java
@@ -0,0 +1,16 @@
+package com.example.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContainerService {
+
+
+
+
+
+
+
+
+}
